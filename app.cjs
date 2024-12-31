864f2936ac5959637bb94158bd81f1802033173d
Administrator <huchen0330@gmail.com>
2024-12-31 14:29:08 +0800

    增加YEAH平台解析，优化代理访问

--- app.cjs
+++ app.cjs
@@ -3,7 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const { URL } = require('url');
 const app = express();
-const port = 3000;
+const port = 3001;
 const { getBonusArriveRedirectUrl } = require('./util3nextAndreplace.js')
 
 
